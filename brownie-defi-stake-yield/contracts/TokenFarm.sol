ecd11c96f355230c9bcc4dbddcfd35e4edc51087
0xalphaprime <0xalphaprime@gmail.com>
2022-11-29 06:25:41 -0500

    112922 @ 13:39:43

--- brownie-defi-stake-yield/contracts/TokenFarm.sol
+++ brownie-defi-stake-yield/contracts/TokenFarm.sol
@@ -30,7 +30,7 @@ contract TokenFarm is Ownable {
     function issueTokens() public onlyOwner {
         for (
             uint256 stakersIndex = 0;
-            stakersIndex <= stakersIndex.length;
+            stakersIndex <= stakers.length;
             stakersIndex++
         ) {
             address recipient = stakers[stakersIndex];
@@ -42,7 +42,7 @@ contract TokenFarm is Ownable {
     function getUserTotalValue(address _user) public returns (uint256) {
         uint256 totalValue = 0;
         require(
-            uniqueTokensStakeds[_user] > 0,
+            uniqueTokensStaked[_user] > 0,
             "You don't have any tokens staked."
         );
         for (
@@ -100,19 +100,19 @@ contract TokenFarm is Ownable {
             stakingBalance[_token][msg.sender] +
             _amount;
         if (uniqueTokensStaked[msg.sender] == 1) {
-            stakers.push[msg.sender];
+            stakers.push(msg.sender);
         }
     }
 
     function unstakeTokens(address _token) public {
-        uint256 balance = stakeBalance[_token][msg.sender];
+        uint256 balance = stakingBalance[_token][msg.sender];
         require(balance > 0, "Staking Balance is 0.");
         IERC20(_token).transfer(msg.sender, balance);
         stakingBalance[_token][msg.sender] = 0;
         uniqueTokensStaked[msg.sender] = uniqueTokensStaked[msg.sender] - 1;
     }
 
-    function updateUniqueTokenStaked(address user, address token) internal {
+    function updateUniqueTokenStaked(address _user, address _token) internal {
         if (stakingBalance[_token][_user] <= 0) {
             uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
         }
